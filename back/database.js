43808405d02b432dd6cadf371599bed4666e46df
unknown <qj_joy@yahoo.com>
2023-06-23 18:50:17 -0400

    generated initial users

--- back/database.js
+++ back/database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: 'postgress',
-    password: 'Naruto',
+    password: process.env.POSTGRESS,
     host: 'localhost',
     port: '5432',
     database: 'QuantumQuorum'
